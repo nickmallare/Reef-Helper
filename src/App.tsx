9da434e3eb6c311b97a97ddd8ccf9124a3c17460
Nick Mallare <nicholasmallare@gmail.com>
2020-11-03 21:48:25 -0500

    Alk conversion
    Added the formula for converting Alkalinty from ppm to dkh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, { useRef, useState } from 'react';
+import { IonAlert, IonApp, IonButton, IonCard, IonCardContent, IonCardHeader, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRouterOutlet, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { calculatorOutline, refreshOutline, flaskOutline } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,16 +20,124 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { isCallSignatureDeclaration } from 'typescript';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+
+const App: React.FC = () => {
+  const [calculatedAlk, setCalculatedAlk] = useState<number>();
+  const [calculatedPhos, setCalculatedPhos] = useState<number>();
+  const [alkError, setAlkError] = useState<string>();
+  const [phosphateError, setPhosphateError] = useState<boolean>();
+  const [showHideAlk, setShowHideAlk] = useState(false);
+  const [showHidePhos, setShowHidePhos] = useState(false);
+
+  const alkInputRef = useRef<HTMLIonInputElement>(null);
+  const phosphateInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateAlk = () => {
+    const enteredAlk = alkInputRef.current!.value;
+    if (!enteredAlk) {
+      console.log("alk is null");
+      return;
+    }
+    const alk = 0.056 * +enteredAlk;
+    setCalculatedAlk(alk);
+
+    if (alk > 11.0) {
+      setAlkError('Your Alk is very high, it is recommended that you lower it.')
+      console.log('greater than 11');
+    }
+    else if (alk < 6.5) {
+      setAlkError('Your Alk is very low, it is recommended that you raise it.')
+      console.log('lower than 6.5');
+    }
+  };
+
+  const calculatePhosphate = () => {
+    const enteredPhos = phosphateInputRef.current!.value;
+    if (!enteredPhos) {
+      console.log("phosphate is null")
+      return;
+    }
+    const phosphate = (3.066 * +enteredPhos) / 1000;
+    setCalculatedPhos(phosphate);
+  };
+
+  function calculateReadings() {
+    calculatePhosphate();
+    calculateAlk();
+    console.log("caluclateReadings");
+  }
+
+  const resetInputs = () => {
+    alkInputRef.current!.value = '';
+    phosphateInputRef.current!.value = '';
+  };
+
+  return (
+    <React.Fragment>
+      <IonAlert isOpen={!!alkError}
+        onDidDismiss={() => setAlkError('')}
+        message={alkError}
+        buttons={['OK']}></IonAlert>
+      <IonApp>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Reef Calculators</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonGrid>
+            <IonRow>
+              <IonCol size-sm>
+              <IonCard>
+                <IonButton onClick={() => (setShowHideAlk(!showHideAlk))}>Alkalinty
+                <IonIcon icon={flaskOutline}/>
+                </IonButton>
+                {showHideAlk &&
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonInput placeholder="Enter Alkalinty in PPM" ref={alkInputRef}/>
+                    </IonCol>
+                    <IonCol>
+                      <IonButton className="cardButtons" onClick={calculateAlk}>
+                        Convert
+                        <IonIcon icon={calculatorOutline}/>
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
+                  <IonRow>
+                    <h2>{calculatedAlk} </h2>
+                  </IonRow>
+              </IonGrid>
+                }
+              </IonCard>
+              </IonCol>
+           
+              <IonCol  size-sm>
+              <IonCard>
+                  <IonButton onClick={() => (setShowHidePhos(!showHidePhos))}>Phosphate
+                  <IonIcon icon={calculatorOutline}/>
+                  </IonButton>
+                {showHidePhos &&
+                <IonGrid>
+                  <IonRow>
+                    <IonInput placeholder="Enter Phosphate in PPB" ref={phosphateInputRef}></IonInput>
+                    <IonLabel></IonLabel>
+                  </IonRow>
+                </IonGrid>
+                }
+           
+              </IonCard>
+              </IonCol>
+            </IonRow>
+
+          </IonGrid>
+        </IonContent>
+      </IonApp>
+    </React.Fragment>
+  );
+};
 
 export default App;
